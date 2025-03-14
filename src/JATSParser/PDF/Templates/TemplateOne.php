b5899db4aa4c24148294961b6cc039f8c79b679c
Tomas Messineo <tomasnahuelmessineo@gmail.com>
2025-03-14 12:08:50 -0300

    save changes

--- src/JATSParser/PDF/Templates/TemplateOne.php
+++ src/JATSParser/PDF/Templates/TemplateOne.php
@@ -520,10 +520,23 @@ class TemplateOne extends \TCPDF {
 	}
 
 	public function printJournalLogo(Array $journalLogoConfig): void {
-		$path = $journalLogoConfig['journal_logo_public_path'];
-		if ($path && file_exists($path)) {
-			$this->Image($path, $journalLogoConfig['x_pos'], $journalLogoConfig['y_pos'], $journalLogoConfig['width']);
+		//Verify if a journal logo exists in a specific directory:
+		$logoPath = null;
+		$logoFile = glob($journalLogoConfig['journal_logo_path'] . "logo.*");
+		if (!empty($logoFile)) {
+			$logoPath = $logoFile[0];
 		}
-	}
+				
+		// If the specific journal logo exists in the "/var/www/files/journals/{journal_id}" directory, process that logo
+		if ($logoPath && file_exists($logoPath)) {
+			$imgtype = \TCPDF_IMAGES::getImageFileType($logoPath);
+			if (($imgtype === 'eps') OR ($imgtype === 'ai')) {
+				$this->ImageEps($logoPath, $journalLogoConfig['x_pos'], $journalLogoConfig['y_pos'], $journalLogoConfig['width']);
+			} elseif ($imgtype === 'svg') {
+				$this->ImageSVG($logoPath, $journalLogoConfig['x_pos'], $journalLogoConfig['y_pos'], $journalLogoConfig['width']);
+			} else {
+				$this->Image($logoPath, $journalLogoConfig['x_pos'], $journalLogoConfig['y_pos'], $journalLogoConfig['width']);
+			}
+		}
 
 }
