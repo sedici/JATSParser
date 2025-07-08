0408915e304c4bc6034bb737e6443f3626719340
Tomas Messineo <tomasnahuelmessineo@gmail.com>
2025-07-08 10:05:59 -0300

    fixed null values in TitlesAndSubtitles.php

--- src/JATSParser/PDF/Templates/Renderers/GroupRenderer/TitlesAndSubtitles.php
+++ src/JATSParser/PDF/Templates/Renderers/GroupRenderer/TitlesAndSubtitles.php
@@ -5,19 +5,23 @@ use JATSParser\PDF\Templates\Renderers\SingleRenderer\NoLinkableText;
 class TitlesAndSubtitles {
     public static function renderTitlesAndSubtitles($pdfTemplate, float $x, float $y, Array $titlesConfig, Array $subtitlesConfig, $localeKey) {
         $pdfTemplate->SetXY($x, $y);
-            
-        $pdfTemplate->SetFont($titlesConfig['titles_config']['principal_title_font']['family'], $titlesConfig['titles_config']['principal_title_font']['style'], $titlesConfig['titles_config']['principal_title_font']['size']);
-        $pdfTemplate->SetTextColor($titlesConfig['titles_config']['principal_title_color'][0], $titlesConfig['titles_config']['principal_title_color'][1], $titlesConfig['titles_config']['principal_title_color'][2]);
-        $pdfTemplate->Write(5, $titlesConfig['titles_texts'][$localeKey]);
-        unset($titlesConfig['titles_texts'][$localeKey]);
+        
+        if ($titlesConfig['titles_texts'] && $titlesConfig['titles_texts'][$localeKey]) {
+            $pdfTemplate->SetFont($titlesConfig['titles_config']['principal_title_font']['family'], $titlesConfig['titles_config']['principal_title_font']['style'], $titlesConfig['titles_config']['principal_title_font']['size']);
+            $pdfTemplate->SetTextColor($titlesConfig['titles_config']['principal_title_color'][0], $titlesConfig['titles_config']['principal_title_color'][1], $titlesConfig['titles_config']['principal_title_color'][2]);
+            $pdfTemplate->Write(5, $titlesConfig['titles_texts'][$localeKey]);
+            unset($titlesConfig['titles_texts'][$localeKey]);
+        }
 
         $pdfTemplate->Ln(7);
  
-        $pdfTemplate->SetFont($subtitlesConfig['subtitles_config']['principal_subtitle_font']['family'], $subtitlesConfig['subtitles_config']['principal_subtitle_font']['style'], $subtitlesConfig['subtitles_config']['principal_subtitle_font']['size']);
-        $pdfTemplate->SetTextColor($subtitlesConfig['subtitles_config']['principal_subtitle_color'][0], $subtitlesConfig['subtitles_config']['principal_subtitle_color'][1], $subtitlesConfig['subtitles_config']['principal_subtitle_color'][2]);
-        $pdfTemplate->Write(5, $subtitlesConfig['subtitles_texts'][$localeKey]);
-        unset($subtitlesConfig['subtitles_texts'][$localeKey]);
-
+        if ($titlesConfig['titles_texts'] && $titlesConfig['titles_texts'][$localeKey]) {
+            $pdfTemplate->SetFont($subtitlesConfig['subtitles_config']['principal_subtitle_font']['family'], $subtitlesConfig['subtitles_config']['principal_subtitle_font']['style'], $subtitlesConfig['subtitles_config']['principal_subtitle_font']['size']);
+            $pdfTemplate->SetTextColor($subtitlesConfig['subtitles_config']['principal_subtitle_color'][0], $subtitlesConfig['subtitles_config']['principal_subtitle_color'][1], $subtitlesConfig['subtitles_config']['principal_subtitle_color'][2]);
+            $pdfTemplate->Write(5, $subtitlesConfig['subtitles_texts'][$localeKey]);
+            unset($subtitlesConfig['subtitles_texts'][$localeKey]);
+        }
+        
         $pdfTemplate->Ln(10);
 
         foreach ($titlesConfig['titles_texts'] as $language => $title) {
